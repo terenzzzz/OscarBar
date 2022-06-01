d7827f8473e7ec3d31761d663bd665a2e192eb51
Zhicong Jiang <terence592592@gmail.com>
2022-06-01 22:58:47 +0100

    price in order

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,10 +2,11 @@
 #
 # Table name: orders
 #
-#  id         :bigint           not null, primary key
-#  user_id    :bigint           not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id          :bigint           not null, primary key
+#  user_id     :bigint           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  total_price :float
 #
 class Order < ApplicationRecord
     belongs_to :user
