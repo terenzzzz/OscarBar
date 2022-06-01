025cf9b4e4b72a53d4937ee07ab11791fd7c9e77
Zhicong Jiang <terence592592@gmail.com>
2022-06-01 22:35:06 +0100

    show orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,6 +5,13 @@ class OrdersController < ApplicationController
         @orders = Order.all
     end
 
+    def full_order
+        @order = Order.find(params[:id])
+        @order_products = @order.order_products
+        @user = User.find(@order.user_id)
+        @rate = Rate.first.exchange_rate
+        @rmb = (@order.total_price * @rate).round(2)
+    end
 
 
 end
