c482d61316e0550da07b72e7d02c9c401ccb10e4
Zhicong Jiang <terence592592@gmail.com>
2022-06-04 01:43:29 +0100

    destroy card before sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
-    # before_action :destroy_cart, only: :destroy
+    before_action :destroy_cart, only: :destroy
 
     def after_sign_in_path_for(resource)
         if current_user.id == 1
